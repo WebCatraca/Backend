f089dba644546865dbcc5dda1b5be871f84201fb
Richard <vinicys13@gmail.com>
2026-06-08 19:49:41 -0300

    conexao com o banco de dados

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+.then(()=> app.emit('Conectado'))
+.catch((err)=> console.err(err))
 
 
-app.get('/', (req,res)=>{
-    res.send('opa tamo aki')
-})
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(routes)
 
-app.listen(3000, ()=>{
-    console.log('http://localhost:3000')
+app.on('Conectado',()=>{
+    app.listen(3000, ()=>{
+        console.log('Servidor ligado')
+        console.log('Acessar http://localhost:3000');
+    })
 })
